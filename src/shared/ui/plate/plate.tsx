72f8549ccbdf23dad2e0b9bfc3f601c5a5cb4350
DenisEgorov200 <egorov.denis.mem@gmail.com>
2024-06-21 21:02:27 +0300

    feat(plate): add shadow and darker border

--- src/shared/ui/plate/plate.tsx
+++ src/shared/ui/plate/plate.tsx
@@ -10,7 +10,7 @@ export const Plate = ({ className, children }: Props) => {
   return (
     <div
       className={clsx(
-        'cursor-pointer rounded-md border border-gray-100 transition-transform hover:translate-y-2',
+        'cursor-pointer rounded-md border border-gray-200 shadow-md transition-transform hover:translate-y-2',
         className,
       )}
     >
